e331671b90bacf679db2e2399ce6db32555357e6
ArisEspino <espinozaariss20@gmail.com>
2025-01-23 16:40:52 -0600

    fixing bugs

--- react-app_2/src/routes/Navigation.tsx
+++ react-app_2/src/routes/Navigation.tsx
@@ -1,5 +1,6 @@
 import logo from '../logo.svg'
-import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, NavLink,  Route, Routes } from 'react-router-dom';
+import { ShoppingPage } from '../02-component-patterns/pages/ShoppingPage';
 
 
 
@@ -7,27 +8,25 @@ export const Navigation = () => {
     return (
         <BrowserRouter >
             <div className="main-layout">
-
-
                 <nav>
                     <img src={logo} alt='React Logo' />
                     <ul>
                         <li>
-                            <NavLink to="/" className={({ isActive }) => isActive ? 'nav-active' : ''}>Home</NavLink>
+                            <NavLink to="/" className={({ isActive }) => isActive ? 'nav-active' : ''}>Shopping</NavLink>
                         </li>
                         <li>
                             <NavLink to="/about" className={({ isActive }) => isActive ? 'nav-active' : ''}>About</NavLink>
                         </li>
                         <li>
-                            <NavLink to="/user" className={({ isActive }) => isActive ? 'nav-active' : ''}>User</NavLink>
+                            <NavLink to="/users" className={({ isActive }) => isActive ? 'nav-active' : ''}>User</NavLink>
                         </li>
                     </ul>
                 </nav>
 
                 <Routes>
-                    <Route path="/" element={<h1>Home</h1>} />
                     <Route path="/about" element={<h1>About</h1>} />
-                    <Route path="/user" element={<h1>User</h1>} />
+                    <Route path="/users" element={<h1>User</h1>} />
+                    <Route path="/" element={<h1><ShoppingPage/></h1>} />
                 </Routes>
             </div>
         </BrowserRouter>
